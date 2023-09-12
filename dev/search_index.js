7a188aba4a3457ca7ccd052e586f72636343fa9a
Documenter.jl <documenter@juliadocs.github.io>
2023-09-12 17:35:21 +0000

    build based on 345cc6c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Introduction-to-Julia","page":"General Information","title":"Introduction to Julia","text":"","category":"section"},{"location":"","page":"General Information","title":"General Information","text":"This is a quick set of introductory materials on Julia for courses taught by James Schloss (Leios).","category":"page"}]
+}
