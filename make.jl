d43b94498c4c721d2e0fd66d0e73baecc8519f90
James Schloss <jrs.schloss@gmail.com>
2024-12-09 15:03:40 +0100

    fixing make.jl

--- make.jl
+++ make.jl
@@ -1,3 +1,4 @@
+module Intro
 using Documenter
 
 makedocs(
@@ -7,9 +8,20 @@ makedocs(
         "Welcome" => "index.md",
         "Reviewer Guidelines" => "content/reviewers.md",
         "About the Author" => "content/about_me.md",
+        #"Development Environments" => "ides.md",
+        #"Packages" => "packages.md",
+        #"General Syntax" => "syntax.md",
+        #"General Development" => "development.md",
+        #"Plotting" => "plotting.md",
+        #"Parallelization" => "parallel.md",
+        #"Performance Notes" => "performance.md",
+        #"Benchmarking" => "benchmarks.md",
+        #"Debugging" => "debug.md",
+        #"Other Questions" => "questions.md",
     ],
 )
 
 deploydocs(;
     repo="github.com/leios/Intro.jl",
 )
+end
